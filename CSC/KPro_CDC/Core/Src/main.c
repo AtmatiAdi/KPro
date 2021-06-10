87dd6c41606814488945fc241b1f3752a9d10272
AtmatiAdi <32931612+AtmatiAdi@users.noreply.github.com>
2021-06-10 19:08:31 +0200

    preparations for dma RX

--- CSC/KPro_CDC/Core/Src/main.c
+++ CSC/KPro_CDC/Core/Src/main.c
@@ -46,6 +46,7 @@ SPI_HandleTypeDef hspi2;
 
 UART_HandleTypeDef huart1;
 DMA_HandleTypeDef hdma_usart1_tx;
+DMA_HandleTypeDef hdma_usart1_rx;
 
 /* USER CODE BEGIN PV */
 char UART_Received[2];
@@ -274,6 +275,9 @@ static void MX_DMA_Init(void)
   /* DMA1_Channel4_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
+  /* DMA1_Channel5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
 
 }
 
